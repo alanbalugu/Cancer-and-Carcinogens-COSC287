ec9a529e1416fbc310a2f7b397ece066368f4231
chau-le <56098357+chau-le@users.noreply.github.com>
2019-11-09 16:38:16 -0500

    Add files via upload

---
+++ CDC/CDC_uscs_and_Predictive_Models_AlanChau.py
@@ -0,0 +1,271 @@
+# Python code for analyzing USCS data
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+import csv
+from pandas.plotting import scatter_matrix
+from pprint import pprint
+from scipy.stats import zscore
+from sklearn import preprocessing
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from pprint import pprint
+
+# calculate mean, mode (for categorical data), median,
+# and std for at least 10 attributes, 3 attributes here 
+def basic_statistical_analysis(myData):
+	myData = myData.dropna(axis=0)
+	myData = myData.drop(columns=['CancerType'])
+	myData['CaseCount'] = pd.to_numeric(myData['CaseCount'])
+
+	# the pandas describe method calculates mean and std
+	# as well as other relevant values like quartile, min,
+	# and max
+	print(myData.describe())
+
+	# median for the 3 main numeric attributes we're analyzing
+	# in the USCS dataset
+	print("\n" + "Median for Age Adjusted Rate: " + str(myData['AgeAdjustedRate'].median()))
+	print("\n" + "Median for Age Case Count: " + str(myData['CaseCount'].median()))
+	print("\n" + "Median for Population: " + str(myData['Population'].median()) + "\n")
+
+	return myData
+
+# find outliers in data by column using z score, but keep for analysis
+def find_outliers(myData):
+#	pprint(myData)
+	# not necessary to work with all attributes in the
+	# data frame, these three are the most relevant
+	cols = ['AgeAdjustedRate', 'CaseCount', 'Population']
+
+	# using scipy package to get z score
+	z_scores = myData[cols].apply(zscore)
+	print("Z scores for AgeAdjustedRate, CaseCount, and Population" + "\n")
+	pprint(z_scores)
+
+	# tally of outliers in each column, we use -2.5 and 2.5 z score
+	# as the lower and upper bounds for determining if outlier or not
+	total_outliers_AAR = 0
+	total_outliers_CC = 0
+	total_outliers_P = 0
+
+	AAR_list = z_scores['AgeAdjustedRate'].tolist()
+	CC_list = z_scores['CaseCount'].tolist()
+	P_list = z_scores['Population'].tolist()
+
+	for i in AAR_list:
+		if (i < -2.5) | (i > 2.5):
+			total_outliers_AAR += 1
+
+	for i in CC_list:
+		if (i < -2.5) | (i > 2.5):
+			total_outliers_CC += 1
+
+	for i in P_list:
+		if (i < -2.5) | (i > 2.5):
+			total_outliers_P += 1
+
+	print("total_outliers_AAR: " + str(total_outliers_AAR) + "\n")
+	print("total_outliers_CC: " + str(total_outliers_CC) + "\n")
+	print("total_outliers_P: " + str(total_outliers_P) + "\n")
+
+def normalize_data(myData):
+	non_numerical_data = ['Area']
+	myData[non_numerical_data] = myData[non_numerical_data].apply(LabelEncoder().fit_transform)
+
+	# normalizing data, this operation is only done on continuous attributes
+	attributes_to_normalize = ['AgeAdjustedRate', 'lci', 'uci', 'CaseCount', 'Population']
+	myData[attributes_to_normalize] = myData[attributes_to_normalize].apply(lambda x:(x-x.min())/(x.max()-x.min()))
+
+# normalizing merged dataset for predictive models
+def normalize_merged_data(myData):
+	categorical_vars = ['STATE_ABBR', 'CHEM_RATE_LEVEL', 'CANCER_RATE_LEVEL']
+	attributes_to_normalize = ['YEAR', 'AVG_REL_EST_TOTAL', 'AGE_ADJUSTED_CANCER_RATE', 'Z_SCORE_AVG_REL_EST_TOTAL', 'Z_SCORE_AGE_ADJUSTED_CANCER_RATE']
+	myData[categorical_vars] = myData[categorical_vars].apply(LabelEncoder().fit_transform)
+	myData[attributes_to_normalize] = myData[attributes_to_normalize].apply(lambda x:(x-x.min())/(x.max()-x.min()))
+
+	return myData
+
+# call on merged CDC and EPA dataframe
+# using z-score, determine if rate of cancer and toxin release in a state
+# is very low (below -2.5 inclusive), low (-2.5 to -1 inclusive), 
+# medium (-1 to 1 inclusive), high (1 to 2.5 inclusive), and very high (above 2.5)
+# also creates 2 new columns for the categorizing the levels
+def get_category_of_cancer_rate(myData):
+	cols = ['AVG_REL_EST_TOTAL', 'AGE_ADJUSTED_CANCER_RATE']
+
+	# drops rows that don't have cancer data
+	myData = myData[myData['AGE_ADJUSTED_CANCER_RATE'].notnull()]
+
+	z_scores = myData[cols].apply(zscore)
+
+	chem_list = z_scores['AVG_REL_EST_TOTAL'].tolist()
+	cancer_list = z_scores['AGE_ADJUSTED_CANCER_RATE'].tolist()
+
+	myData['Z_SCORE_AVG_REL_EST_TOTAL'] = chem_list
+	myData['Z_SCORE_AGE_ADJUSTED_CANCER_RATE'] = cancer_list
+
+	# this will be either very low, low, medium, etc
+	cancer_rate_level = []
+	chem_rate_level = []
+	x = " "
+
+	for i in cancer_list:
+		if (i <= -2.5):
+			cancer_rate_level.append("very low")
+		if ((i > -2.5) & (i <= -1)):
+			cancer_rate_level.append("low")
+		if ((i > -1) & (i <= 1)):
+			cancer_rate_level.append("medium")
+		if ((i > 1) & (i <= 2.5)):
+			cancer_rate_level.append("high")
+		if (i > 2.5):
+			cancer_rate_level.append("very_high")
+
+	for i in chem_list:
+		if (i <= -2.5):
+			chem_rate_level.append("very low")
+		if ((i > -2.5) & (i <= -1)):
+			chem_rate_level.append("low")
+		if ((i > -1) & (i <= 1)):
+			chem_rate_level.append("medium")
+		if ((i > 1) & (i <= 2.5)):
+			chem_rate_level.append("high")
+		if (i > 2.5):
+			chem_rate_level.append("very_high")
+
+	myData['CHEM_RATE_LEVEL'] = chem_rate_level
+	myData['CANCER_RATE_LEVEL'] = cancer_rate_level
+
+	print("Z scores for AGE_ADJUSTED_CANCER_RATE" + "\n")
+	pprint(z_scores)
+	pprint(myData)
+
+	return myData
+
+# makes naive bayes and random forest classifiers
+# analyzes merged cdc and epa data
+def predictive_models(myData, numAttributes):
+	scoring = 'accuracy'
+	valueArray = myData.values
+	X = valueArray[:, 0:numAttributes]
+	Y = valueArray[:, numAttributes]
+	test_size = 0.20
+	seed = 7
+	X_train, X_validate, Y_train, Y_validate = train_test_split(X, Y, test_size=test_size, random_state=seed)
+
+	# gaussian naive bayes classifier
+	gaussian_nb = GaussianNB().fit(X_train, Y_train)
+	random_forest = RandomForestClassifier(n_estimators = 50, random_state=12).fit(X_train, Y_train)
+
+	models = []
+
+	models.append(('random_forest', RandomForestClassifier()))
+	models.append(('gaussian_nb', GaussianNB()))
+
+	results = []
+	names = []
+
+	for name, model in models:
+		model.fit(X_train, Y_train)
+		x_predictions =  model.predict(X_validate) # etc
+		print(accuracy_score(Y_validate, x_predictions))
+		print(confusion_matrix(Y_validate, x_predictions))
+		print(classification_report(Y_validate, x_predictions))
+
+#makes a line graph and adds to plot
+def lineGraphByRegion(x_data, y_data, color_val, region, y_axis_label, title_label, state):
+
+	#plt.xticks(x_data)
+	fig = plt.figure(1)
+	ax = plt.axes()
+	plt.title(title_label)
+	plt.xlabel('X - Axis')
+	plt.ylabel(y_axis_label)
+	ax.plot(x_data, y_data, color = color_val, label = region)
+	handles, labels = ax.get_legend_handles_labels()
+	#lgd = ax.legend(handles, labels, loc="center right", bbox_to_anchor=(1, 0.5))
+	plt.text(x_data[-1], y_data[-1], state, color = "red")
+	ax.grid('on')
+
+# plots data	
+def plot_data(myData):
+	# Box and whisker plots
+	myData.plot(kind='box', subplots=True, layout=(2,4), sharex=False, sharey=False)
+	plt.tight_layout()
+	plt.show()
+
+	# Histogram
+	myData.hist(bins=10)
+	plt.tight_layout()
+	plt.show()
+
+	for column in myData.columns:
+		try:
+			myData[column].hist(bins=(myData[column].max()+1 - myData[column].min()))
+			plt.xticks(np.arange(myData[column].min(), myData[column].max(), 1))
+			plt.show()
+		except:
+			print("histogram not possible")
+			mean_list = []
+
+			color_counter = 0;
+			colors = ["black"]
+
+			for each in myData['Area'].unique():
+				for yr in range(myData['Year'].min(), myData['Year'].max(), 1):
+					mean_list.append((myData.loc[myData['Area'] == each].loc[myData['Year'] == yr])[column].mean())
+					#print(mean_list)
+				
+				lineGraphByRegion(list(range(myData['Year'].min(), myData['Year'].max())), mean_list, colors[color_counter], each, column, "Mean Values for Each State Over Time", each)
+
+				mean_list.clear()
+				#color_counter += 1
+
+			#plt.clf()
+			plt.show()
+			#plt.clf()
+	
+	# Scatterplots to look at 2 variables at once
+	# scatter plot matrix
+	scatter_matrix(myData)
+	plt.show()
+
+# writes out dataframe
+def write_to_file(myData, file_name):
+	myData.to_csv(file_name)
+
+# main
+def main():
+	df = pd.read_csv('USCS_CancerTrends_OverTime_ByState.csv' , sep=',', encoding='latin1')
+	merged_df = pd.read_csv('merged_data.csv', sep=',', encoding='latin1')
+
+	df = basic_statistical_analysis(df)
+	find_outliers(df)
+	normalized_df = normalize_data(df)
+
+#	plot_data(normalized_df)
+
+	new_df = get_category_of_cancer_rate(merged_df)
+	new_df = new_df.loc[:, new_df.columns.intersection(['YEAR', 'STATE_ABBR', 'AVG_REL_EST_TOTAL', 'AGE_ADJUSTED_CANCER_RATE', 'Z_SCORE_AVG_REL_EST_TOTAL', 'Z_SCORE_AGE_ADJUSTED_CANCER_RATE', 'CHEM_RATE_LEVEL', 'CANCER_RATE_LEVEL'])]
+	new_df = normalize_merged_data(new_df)
+
+	predictive_models(new_df, 7)
+	print("end main")
+
+if __name__ == '__main__':
+    main()
+
